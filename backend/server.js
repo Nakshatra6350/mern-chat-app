e96763908810a5b1450d64c35a200320992bf04e
Nakshatra6350 <nakshatragarg678@gmail.com>
2024-02-10 02:12:02 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,25 @@ import authRoutes from './routes/auth.routes.js'
 import connectToMongoDB  from './db/connectToMongodb.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
+import path from 'path';
 import {app,server} from './socket/socket.js'
 // const app = express();
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
